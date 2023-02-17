28749d7f43b89612eb936d7b035f1478d358e2e2
bt-koch <bela.koch@hotmail.com>
2023-02-17 10:56:54 +0100

    file paths corrected

--- combine_datasets.R
+++ combine_datasets.R
@@ -2,8 +2,8 @@ rm(list = ls()); gc()
 library(data.table)
 
 # load csv files
-data_old <- read.csv("kaggle_dataset.csv")
-data_new <- read.csv("classictrader.csv")
+data_old <- read.csv("classictrader.csv")
+data_new <- read.csv("classictrader_newdata.csv")
 
 # combine data, filter out duplicates
 data <- rbind(data_old, data_new)
